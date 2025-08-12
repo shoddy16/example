53a2a072fdb56398b238c7bb1091ee39546eac9d
shoddy16 <142566890+shoddy16@users.noreply.github.com>
2025-08-12 18:37:51 +0530

    Add files via upload

---
+++ array_1.py
@@ -0,0 +1,21 @@
+
+# def search(stock,n):
+#  for i in range(len(stock)):
+#   if stock[i]==n:
+#     return i
+#  else:
+#   return False
+
+# stock=[200,455,780,255,700]
+# n=int(input("Enter a no:"))
+
+# if search(stock,n):
+#  print("Element found ")
+# else:
+#  print("Element not found")
+
+
+
+stock=[30,40,50,70,10]
+stock.insert(4,87)
+print (stock)
